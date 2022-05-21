ba10249ca1b79a7f76956fc4c6b5da789dfdde97
suha <hp980724@gmail.com>
2022-05-22 00:20:13 +0900

    2022-05-21 개념 완료 프로젝트 시작

---
+++ html,css,js 연습/표준 내장 객체.js
@@ -0,0 +1,19 @@
+// /* 표준 내장 객체 */
+// const str = '  abc cba  ';
+// console.log(str)
+// console.log(str.length)
+// console.log(str.includes('a'))
+// console.log(str.indexOf('a'))
+// console.log(str.trim())
+// console.log(str.replaceAll(' ', ''));
+
+
+// const arr = ['a', 'b', 'c '];
+// arr.push('d')
+// console.log(arr)
+// arr.forEach(function (v) {
+//     console.log(v);
+// })
+
+const random = Math.random()*20;
+console.log(Math.floor(random));
